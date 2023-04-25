1aa1279ac82c1d13e6a0809c4f454ef2b25b8fed
Benoît Hubert <benoithubert@gmail.com>
2023-04-26 01:35:18 +0200

    force expo start url

--- e2e/tests/utils/openApp.js
+++ e2e/tests/utils/openApp.js
@@ -15,12 +15,16 @@ module.exports.openApp = async function openApp() {
 };
 
 async function openAppForDebugBuild(platform) {
-  const deepLinkUrl = process.env.EXPO_USE_UPDATES
+  /*const deepLinkUrl = process.env.EXPO_USE_UPDATES
     ? // Testing latest published EAS update for the test_debug channel
       getDeepLinkUrl(getLatestUpdateUrl())
     : // Local testing with packager
       getDeepLinkUrl(getDevLauncherPackagerUrl(platform));
-
+  */
+  // const deepLinkUrl = 'com.reactnativedevfr.eastestsexample://expo-development-client/?url=http%3A%2F%2F192.168.1.67%3A8081%2Findex.bundle%3Fplatform%3Dandroid%26dev%3Dtrue%26minify%3Dfalse%26disableOnboarding%3D1';
+  //const deepLinkUrl = 'com.reactnativedevfr.eastestsexample://expo-development-client/?url=http%3A%2F%2F192.168.1.67%3A8081%2F%3Fplatform%3Dandroid%26dev%3Dtrue%26minify%3Dfalse%26disableOnboarding%3D1';
+  const deepLinkUrl = 'com.reactnativedevfr.eastestsexample://expo-development-client/?url=http%3A%2F%2F192.168.1.67%3A8081';
+  console.log('>>>> deepLinkUrl:', deepLinkUrl);
   if (platform === 'ios') {
     await device.launchApp({
       newInstance: true,
